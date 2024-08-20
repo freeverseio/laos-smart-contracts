5c70daefe42abc956fe7892951c33d9a4699c0c0
Toni <>
2024-08-20 17:00:36 +0300

    contract done

--- contracts/LaosPublicMinter.sol
+++ contracts/LaosPublicMinter.sol
@@ -7,13 +7,44 @@ import "./Ownable.sol";
 contract LaosPublicMinter is Ownable, EvolutionCollection {
 
     address public precompileAddress;
+    bool public isPublicMintingEnabled;
 
-    mapping(address => bool) public whitelisted;
+    modifier onlyOwnerOrPublicMinting() {
+        require(owner() == msg.sender || isPublicMintingEnabled, "Not authorized");
+        _;
+    }
 
     constructor(address _ownerOfPublicMinter) Ownable(_ownerOfPublicMinter) {}
 
-    function setPrecompileAddress(address _newAddress) public {
+    function setPrecompileAddress(address _newAddress) public onlyOwner {
         precompileAddress = _newAddress;
+    }
+
+    function mintWithExternalURI (
+        address _to,
+        uint96 _slot,
+        string calldata _tokenURI
+    ) external onlyOwnerOrPublicMinting returns (uint256) {
+        return EvolutionCollection(precompileAddress).mintWithExternalURI(_to, _slot, _tokenURI);
+    }
+
+    function evolveWithExternalURI(
+        uint256 _tokenId,
+        string calldata _tokenURI
+    ) external onlyOwner {
+         EvolutionCollection(precompileAddress).evolveWithExternalURI(_tokenId, _tokenURI);
+    }
+
+    function transferOwnership(address _newOwner) external onlyOwner {
+        EvolutionCollection(precompileAddress).transferOwnership(_newOwner);
+    }
+
+    function enablePublicMinting() external onlyOwner {
+        isPublicMintingEnabled = true;
+    }
+
+    function disablePublicMinting() external onlyOwner {
+        isPublicMintingEnabled = false;
     }
 
     function owner() public view returns (address){
@@ -22,25 +53,5 @@ contract LaosPublicMinter is Ownable, EvolutionCollection {
 
     function tokenURI(uint256 _tokenURI) external view returns (string memory) {
         return EvolutionCollection(precompileAddress).tokenURI(_tokenURI);
-    }
-
-    function mintWithExternalURI(
-        address _to,
-        uint96 _slot,
-        string calldata _tokenURI
-    ) external returns (uint256) {
-        require(whitelisted[msg.sender], "Sender is not whitelisted");
-        return EvolutionCollection(precompileAddress).mintWithExternalURI(_to, _slot, _tokenURI);
-    }
-
-    function evolveWithExternalURI(
-        uint256 _tokenId,
-        string calldata _tokenURI
-    ) external {
-         EvolutionCollection(precompileAddress).evolveWithExternalURI(_tokenId, _tokenURI);
-    }
-
-    function transferOwnership(address _newOwner) external {
-        EvolutionCollection(precompileAddress).transferOwnership(_newOwner);
     }
 }
