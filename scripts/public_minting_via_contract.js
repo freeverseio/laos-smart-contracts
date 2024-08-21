f43849d3bcc5987b62d3a6c5b66a8b691e855992
Toni <>
2024-08-21 12:28:11 +0300

    using a pattern that makes sure that TXs that should revert actually revert, and retries in case of node refusing to connect

--- scripts/public_minting_via_contract.js
+++ scripts/public_minting_via_contract.js
@@ -6,6 +6,36 @@ const truffleAssert = require('truffle-assertions');
 const createCollectionAddress = "0x0000000000000000000000000000000000000403";
 const maxGas = 5000000;
 
+async function tryWithRetries(func, retries = 3) {
+  for (let i = 0; i < retries; i++) {
+    try {
+      console.log('...attempt: ', i);
+      await func();
+      console.log('...attempt: ', i, 'failed as expected');
+      return; // if the function succeeds, exit
+    } catch (error) {
+      if (error.message.includes('revert')) {
+        throw error; // If the error is a revert, we throw it
+      }
+      console.log(`Retrying... (${i + 1}/${retries})`);
+      if (i === retries - 1) throw error; // after max retries, throw the error
+    }
+  }
+}
+
+async function assertReverts(func) {
+  // Sometimes the failure to exeecture the TX is not due to a revert of the code,
+  // but, e.g., to the node refusing connection, etc.
+  // This code makes sure that the TX fails due to a revert, otherwise tries a few more
+  // times until it truly reverts.
+  await tryWithRetries(async () => {
+    await truffleAssert.fails(
+        func,
+        truffleAssert.ErrorType.REVERT,
+    );
+  });
+}
+
 function random32bit() {
   return Math.floor(Math.random() * Math.pow(2, 32));
 }
@@ -38,10 +68,7 @@ module.exports = async (callback) => {
     await mint(precompileContract, alice, bob);
 
     console.log('bob cannot mint...');
-    await truffleAssert.fails(
-      mint(precompileContract, bob, alice),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(mint(precompileContract, bob, alice));
 
     console.log('deploying publicMinter...');
     const publicMinter = await LaosPublicMinter.new(alice);
@@ -59,22 +86,13 @@ module.exports = async (callback) => {
     console.log('Precompile owner can be queried via publicMinter and matches direct query? ', await precompileContract.owner() === publicMinter.address);
 
     console.log('Bob cannot mint using the publicMinter...');
-    await truffleAssert.fails(
-      mint(publicMinter, bob, alice),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(mint(publicMinter, bob, alice));
 
     console.log('Bob cannot mint using the precompile neither...');
-    await truffleAssert.fails(
-      mint(precompileContract, bob, alice),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(mint(precompileContract, bob, alice));
 
     console.log('alice cannot mint using the precompile neither...');
-    await truffleAssert.fails(
-      mint(precompileContract, alice, bob),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(mint(precompileContract, alice, bob));
 
     console.log('alice owns public minter?...',  alice == await publicMinter.publicMinterOwner());
 
@@ -82,10 +100,7 @@ module.exports = async (callback) => {
     await mint(publicMinter, alice, bob);
 
     console.log('bob is not authorized to enable public minting...');
-    await truffleAssert.fails(
-      publicMinter.enablePublicMinting({from: bob, gas: maxGas}),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(publicMinter.enablePublicMinting({from: bob, gas: maxGas}));
 
     console.log('alice enables public minting...');
     await publicMinter.enablePublicMinting();
@@ -95,16 +110,10 @@ module.exports = async (callback) => {
     await mint(publicMinter, bob, alice);
 
     console.log('bob cannot mint using the precompile...');
-    await truffleAssert.fails(
-      mint(precompileContract, bob, alice),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(mint(precompileContract, bob, alice));
 
     console.log('bob cannot transfer public minting ownership...');
-    await truffleAssert.fails(
-      publicMinter.transferPublicMinterOwnership(bob, {from: bob, gas: maxGas}),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(publicMinter.transferPublicMinterOwnership(bob, {from: bob, gas: maxGas}));
 
     console.log('alice transfers public minting ownership to bob...');
     await publicMinter.transferPublicMinterOwnership(bob);
@@ -112,34 +121,22 @@ module.exports = async (callback) => {
     console.log('precompileContract owner has changed as expected?... ', bob === await precompileContract.owner());
 
     console.log('alice cannot disable public minting...');
-    await truffleAssert.fails(
-      publicMinter.disablePublicMinting({from: alice, gas: maxGas}),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(publicMinter.disablePublicMinting({from: alice, gas: maxGas}));
 
     console.log('bob disables public minting...');
     await publicMinter.disablePublicMinting({from: bob, gas: maxGas});
 
     console.log('alice cannot mint anymore...');
-    await truffleAssert.fails(
-      mint(publicMinter, alice, bob),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(mint(publicMinter, alice, bob));
 
     console.log('bob can since he owns the publicMinter...');
     await mint(publicMinter, bob, alice);
 
     console.log('alice cannot change the owner of the precompile using the precompile...');
-    await truffleAssert.fails(
-      precompileContract.transferOwnership(alice, {from: alice, gas: maxGas}),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(precompileContract.transferOwnership(alice, {from: alice, gas: maxGas}));
 
     console.log('alice cannot change the owner of the precompile using the publicMinter...');
-    await truffleAssert.fails(
-      publicMinter.transferOwnership(alice, {from: alice, gas: maxGas}),
-      truffleAssert.ErrorType.REVERT,
-    );
+    await assertReverts(publicMinter.transferOwnership(alice, {from: alice, gas: maxGas}));
 
     console.log('bob changes the ownership of the precompile using the publicMinter...');
     await publicMinter.transferOwnership(alice, {from: bob, gas: maxGas});
