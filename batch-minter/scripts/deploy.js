312fa6be24b97dcd09bd8c1ab71da82a1167bd66
Toni <>
2024-12-10 17:49:37 +0100

    improving script

--- batch-minter/scripts/deploy.js
+++ batch-minter/scripts/deploy.js
@@ -1,39 +1,25 @@
 const { ethers } = require("hardhat");
 
-const typicalURILength = "ipfs://QmQeN4qhzPpG6jVqJoXo2e86eHYPbFpKeUkJcTfrA5hJwz".length; // = 47
-
 async function main() {
   const [deployer] = await ethers.getSigners();
   const owner = deployer.address; // set your choice of owner, not necessearily the same as the deployer account
 
-  console.log("Deployer's balance (in Wei):", await ethers.provider.getBalance(deployer.address));
-
-  console.log("Deploying batchMinter with owner...", owner);
+  console.log(`Deploying with account ${owner}, with balance (in Wei): ${await ethers.provider.getBalance(deployer.address)}`);
 
   const LaosBatchMinter = await ethers.getContractFactory("LaosBatchMinter");
-  const batchMinter = await LaosBatchMinter.deploy(deployer.address);
+  const batchMinter = await LaosBatchMinter.deploy(owner);
   await batchMinter.waitForDeployment();
-
   console.log("...batchMinter deployed at", await batchMinter.getAddress());
-
-  const batchMinterOwner = await batchMinter.batchMinterOwner();
-  console.log("...batchMinter expected owner?", owner === batchMinterOwner);
+  console.log("...batchMinter has expected owner?", owner === await batchMinter.batchMinterOwner());
 
   const precompileAddress = await batchMinter.precompileAddress();
-  console.log("Precompile address is", precompileAddress);
+  console.log("...batchMinter uses a collection managed by the following precompile address:", precompileAddress);
 
-  const EvolutionCollection = await ethers.getContractAt("EvolutionCollection", precompileAddress);
-  const precompileContract = EvolutionCollection.attach(precompileAddress);
+  const EvolutionCollectionInterface = await ethers.getContractAt("EvolutionCollection", precompileAddress);
+  const precompileContract = EvolutionCollectionInterface.attach(precompileAddress);
 
   const precompileOwner = await precompileContract.owner();
-
-  console.log("...precompileContract owner is batchMinter?", await batchMinter.getAddress() === precompileOwner);
-  console.log(await batchMinter.getAddress(), precompileOwner);
-
-  console.log(
-    "Precompile owner can be queried via batchMinter and matches direct query?",
-    precompileOwner === await batchMinter.getAddress()
-  );
+  console.log("...the owner of the collection at that precompile address is the newly deployed batchMinter?", await batchMinter.getAddress() === precompileOwner);
 }
 
 main().catch((error) => {
