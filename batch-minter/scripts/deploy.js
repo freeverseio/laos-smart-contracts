e96baff364430af831de12858c5525e3300d6e1b
Toni <>
2024-12-10 17:41:30 +0100

    starting deploy script

---
+++ batch-minter/scripts/deploy.js
@@ -0,0 +1,45 @@
+const { ethers } = require("hardhat");
+
+const typicalURILength = "ipfs://QmQeN4qhzPpG6jVqJoXo2e86eHYPbFpKeUkJcTfrA5hJwz".length; // = 47
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const owner = deployer.address; // set your choice of owner, not necessearily the same as the deployer account
+
+  console.log("Deployer's balance (in Wei):", await ethers.provider.getBalance(deployer.address));
+
+  console.log("Deploying batchMinter with owner...", owner);
+
+  const LaosBatchMinter = await ethers.getContractFactory("LaosBatchMinter");
+  const batchMinter = await LaosBatchMinter.deploy(deployer.address);
+  await batchMinter.waitForDeployment();
+
+  console.log("...batchMinter deployed at", await batchMinter.address());
+
+  const batchMinterOwner = await batchMinter.batchMinterOwner();
+  console.log("...batchMinter expected owner?", owner === batchMinterOwner);
+
+  const precompileAddress = await batchMinter.precompileAddress();
+  console.log("Precompile address is", precompileAddress);
+
+  const EvolutionCollection = await ethers.getContractAt("EvolutionCollection", precompileAddress);
+  const precompileContract = EvolutionCollection.attach(precompileAddress);
+
+  const precompileOwner = await precompileContract.owner();
+
+  console.log("...precompileContract owner is batchMinter?", await batchMinter.getAddress() === precompileOwner);
+  console.log(await batchMinter.getAddress(), precompileOwner);
+
+  console.log(
+    "Precompile owner can be queried via batchMinter and matches direct query?",
+    precompileOwner === await batchMinter.getAddress()
+  );
+}
+
+// Execute the script
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
