674f6f6190b32cdf4e716041686f13db8cb5928b
Toni <>
2024-12-11 13:49:45 +0100

    test fixed

--- public-minter/contracts/LaosPublicMinterMinimal.sol
+++ public-minter/contracts/LaosPublicMinterMinimal.sol
@@ -1,14 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity >=0.8.3;
 
+import "./EvolutionCollectionFactory.sol";
 import "./EvolutionCollection.sol";
 import "./Ownable.sol";
 
 contract LaosPublicMinterMinimal is Ownable, EvolutionCollection {
 
+    /**
+     * @dev Emitted on deploy of a new PublicMinter contract
+     * @param _owner the owner of the newly created PublicMinter
+     * @param _precompileAddress the address of the newly created underlying precompiled collection address
+     */
+    event NewPublicMinter(address indexed _owner, address _precompileAddress);
+
+    address public constant collectionFactoryAddress = 0x0000000000000000000000000000000000000403;
     address public precompileAddress;
 
-    constructor(address _ownerOfPublicMinter) Ownable(_ownerOfPublicMinter) {}
+    constructor(address _ownerOfPublicMinter) Ownable(_ownerOfPublicMinter) {
+        precompileAddress = EvolutionCollectionFactory(collectionFactoryAddress).createCollection(address(this));
+        emit NewPublicMinter(_ownerOfPublicMinter, precompileAddress);
+    }
 
     function setPrecompileAddress(address _newAddress) public onlyOwner {
         precompileAddress = _newAddress;
--- public-minter/scripts/test-e2e.js
+++ public-minter/scripts/test-e2e.js
@@ -1,4 +1,9 @@
+// This script performs an end-to-end test for the PublicMinterMinimal contract.
+// It verifies the deployment, ownership transfer, and interactions with the precompile contract.
+// Additionally, it ensures that unauthorized actions are correctly reverted.
+
 const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
 const typicalURILength = "ipfs://QmQeN4qhzPpG6jVqJoXo2e86eHYPbFpKeUkJcTfrA5hJwz".length; // = 47
 const dummyString = 'a'.repeat(typicalURILength);
@@ -18,12 +23,10 @@ async function main() {
 
   console.log(`Deploying with account ${deployer.address}, with balance (in Wei): ${await ethers.provider.getBalance(deployer.address)}`);
 
-  const LaosPublicMinter = await ethers.getContractFactory("LaosPublicMinter");
+  const LaosPublicMinter = await ethers.getContractFactory("LaosPublicMinterMinimal");
   const publicMinter = await LaosPublicMinter.deploy(owner);
   await publicMinter.waitForDeployment();
   console.log("...publicMinter deployed at", await publicMinter.getAddress());
-
-  // Deploy is finished. The remaining code in this script runs sanity checks, verifying that everything is correctly configured. 
 
   console.log("...publicMinter has expected owner?", owner === await publicMinter.publicMinterOwner());
 
@@ -44,9 +47,52 @@ async function main() {
   const receipt = await tx.wait();
   const tokenId1 = receipt.logs[0].args[2].toString();
   console.log('...new tokenId = ', tokenId1);
+
+  console.log('Bob can also mint using the publicMinter...');
+  const publicMinterWithBob = publicMinter.connect(bob);
+  const tx2 = await publicMinter.mintWithExternalURI(bob.address, random32bit(), dummyString);
+  const receipt2 = await tx2.wait();
+  const tokenId2 = receipt2.logs[0].args[2].toString();
+  console.log('...new tokenId = ', tokenId2);
+
+  console.log('Bob cannot mint using the precompile neither...');
+  const precompileWithBob = precompileContract.connect(bob);
+  await expect(
+    precompileWithBob.mintWithExternalURI(bob.address, random32bit(), "teeeeest")
+  ).to.be.reverted;
+
+  console.log('Bob cannot transfer publicMinter ownership...');
+  await expect(
+    publicMinterWithBob.transferPublicMinterOwnership(bob.address)
+  ).to.be.revertedWithCustomError(publicMinterWithBob, "OwnableUnauthorizedAccount")
+  .withArgs(bob.address);
+
+  console.log('Transferring ownership of publicMinter to Bob by deployer...');
+  const tx1 = await publicMinter.transferPublicMinterOwnership(bob.address);
+  await tx1.wait();
+  
+  console.log('PrecompileContract owner remains unchanged as expected?... ', await publicMinter.getAddress() === await precompileContract.owner());
+  console.log('publicMinter owner has changed as expected?... ', bob.address === await publicMinter.publicMinterOwner());
+
+  console.log('Deployer cannot transfer publicMinter ownership anymore...');
+  await expect(
+    publicMinter.transferPublicMinterOwnership(deployer.address)
+  ).to.be.revertedWithCustomError(publicMinterWithBob, "OwnableUnauthorizedAccount")
+  .withArgs(deployer.address); 
+
+  console.log('Deployer cannot change the owner of the precompile using the publicMinter...');
+  await expect(
+    publicMinter.transferOwnership(deployer.address)
+  ).to.be.revertedWithCustomError(publicMinterWithBob, "OwnableUnauthorizedAccount")
+  .withArgs(deployer.address); 
+
+  console.log('Bob can change the ownership of the precompile using the publicMinter...');
+  const tx3 = await publicMinterWithBob.transferOwnership(deployer.address);
+  await tx3.wait();
+  console.log('precompileContract owner is deployer?... ', deployer.address === await precompileContract.owner());
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+  console.error("An unexpected error occurred:", error.message);
+  process.exit(1);
 });
