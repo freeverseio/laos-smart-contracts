ac6e9ff0f7a512d9e6e82ad7bb705ee3a5fa146e
Toni <>
2025-02-13 14:53:38 +0100

    adding precompile test

--- batch-minter-access-control/contracts/test/MockEvolutionCollectionFactory.sol
+++ batch-minter-access-control/contracts/test/MockEvolutionCollectionFactory.sol
@@ -6,6 +6,6 @@ import "../EvolutionCollection.sol";
 contract MockEvolutionCollectionFactory {
 
     function createCollection(address owner) external returns (address) {
-        return address(42);
+        return address(0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512);
     }
 }
--- batch-minter-access-control/test/LaosBatchMinter.test.ts
+++ batch-minter-access-control/test/LaosBatchMinter.test.ts
@@ -12,6 +12,7 @@ describe("LaosBatchMinter", function () {
     let addr2: HardhatEthersSigner;
 
     const fixedCollectionFactoryAddress = "0x0000000000000000000000000000000000000403";
+    const fixedCollectionAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 
     beforeEach(async function () {
@@ -21,13 +22,13 @@ describe("LaosBatchMinter", function () {
         mockFactory = await MockEvolutionCollectionFactory.deploy();
         await mockFactory.waitForDeployment();
 
-        console.log("Mock EvolutionCollectionFactory deployed at:", mockFactory.target);
-        // Force set the contract code at the hardcoded address
         await network.provider.send("hardhat_setCode", [fixedCollectionFactoryAddress, await network.provider.send("eth_getCode", [mockFactory.target])]);
+        console.log("Mock EvolutionCollectionFactory deployed at:", mockFactory.target, 'but network mocked to believe it is at ', fixedCollectionFactoryAddress);
 
         const mockEvolutionCollection = await ethers.getContractFactory("MockEvolutionCollection");
         mockCollection = (await mockEvolutionCollection.deploy(owner.address)) as MockEvolutionCollection;
         await mockCollection.waitForDeployment();
+        console.log("Mock EvolutionCollectionFactory deployed at:", await mockCollection.getAddress());
 
         const LaosBatchMinter = await ethers.getContractFactory("LaosBatchMinter");
         minter = (await LaosBatchMinter.deploy(owner.address)) as LaosBatchMinter;
@@ -37,7 +38,8 @@ describe("LaosBatchMinter", function () {
     it("Should set the correct owner", async function () {
         expect(2).to.equal(2);
         console.log(owner.address);
-        // console.log(await minter.owner());
+        console.log(await minter.precompileAddress());
+        expect(await minter.precompileAddress()).to.equal(fixedCollectionAddress);
         // expect(await minter.owner()).to.equal(owner.address);
     });
 });
