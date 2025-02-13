7417c53c700f7f8183e30f468779d2cc4075c48e
Toni <>
2025-02-13 14:45:48 +0100

    allowing hardhat to use mock

--- batch-minter-access-control/test/LaosBatchMinter.test.ts
+++ batch-minter-access-control/test/LaosBatchMinter.test.ts
@@ -1,26 +1,33 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import { expect } from "chai";
 import { LaosBatchMinter, MockEvolutionCollectionFactory, MockEvolutionCollection } from "../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("LaosBatchMinter", function () {
     let minter: LaosBatchMinter;
-    let mEvolutionCollectionFactory: MockEvolutionCollectionFactory;
-    let mEvolutionCollection: MockEvolutionCollection;
+    let mockFactory: MockEvolutionCollectionFactory;
+    let mockCollection: MockEvolutionCollection;
     let owner: HardhatEthersSigner;
     let addr1: HardhatEthersSigner;
     let addr2: HardhatEthersSigner;
+
+    const fixedCollectionFactoryAddress = "0x0000000000000000000000000000000000000403";
+
 
     beforeEach(async function () {
         [owner, addr1, addr2] = await ethers.getSigners();
 
         const MockEvolutionCollectionFactory = await ethers.getContractFactory("MockEvolutionCollectionFactory");
-        mEvolutionCollectionFactory = await MockEvolutionCollectionFactory.deploy();
-        await mEvolutionCollectionFactory.waitForDeployment();
+        mockFactory = await MockEvolutionCollectionFactory.deploy();
+        await mockFactory.waitForDeployment();
+
+        console.log("Mock EvolutionCollectionFactory deployed at:", mockFactory.target);
+        // Force set the contract code at the hardcoded address
+        await network.provider.send("hardhat_setCode", [fixedCollectionFactoryAddress, await network.provider.send("eth_getCode", [mockFactory.target])]);
 
         const mockEvolutionCollection = await ethers.getContractFactory("MockEvolutionCollection");
-        mEvolutionCollection = (await mockEvolutionCollection.deploy(owner.address)) as MockEvolutionCollection;
-        await mEvolutionCollection.waitForDeployment();
+        mockCollection = (await mockEvolutionCollection.deploy(owner.address)) as MockEvolutionCollection;
+        await mockCollection.waitForDeployment();
 
         const LaosBatchMinter = await ethers.getContractFactory("LaosBatchMinter");
         minter = (await LaosBatchMinter.deploy(owner.address)) as LaosBatchMinter;
