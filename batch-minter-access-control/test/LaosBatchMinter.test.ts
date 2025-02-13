1ad597add35f493000380e9ec5c180a8e0abc2a9
Toni <>
2025-02-13 15:54:42 +0100

    splitting tests

--- batch-minter-access-control/test/LaosBatchMinter.test.ts
+++ batch-minter-access-control/test/LaosBatchMinter.test.ts
@@ -24,24 +24,29 @@ describe("LaosBatchMinter", function () {
         await mockFactory.waitForDeployment();
 
         await network.provider.send("hardhat_setCode", [fixedCollectionFactoryAddress, await network.provider.send("eth_getCode", [mockFactory.target])]);
-        console.log("Mock EvolutionCollectionFactory deployed at:", mockFactory.target, 'but network mocked to believe it is at ', fixedCollectionFactoryAddress);
+        // console.log("Mock EvolutionCollectionFactory deployed at:", mockFactory.target, 'but network mocked to believe it is at ', fixedCollectionFactoryAddress);
 
         const mockEvolutionCollection = await ethers.getContractFactory("MockEvolutionCollection");
         mockCollection = (await mockEvolutionCollection.deploy(owner.address)) as MockEvolutionCollection;
         await mockCollection.waitForDeployment();
-        console.log("Mock EvolutionCollectionFactory deployed at:", await mockCollection.getAddress());
 
         const LaosBatchMinter = await ethers.getContractFactory("LaosBatchMinter");
         minter = (await LaosBatchMinter.deploy(owner.address)) as LaosBatchMinter;
         await minter.waitForDeployment();
     });
 
-    it("Should set the correct intiial parameters", async function () {
+    it("Should set the correct initial parameters", async function () {
         expect(await minter.precompileAddress()).to.equal(fixedCollectionAddress);
         expect(await minter.owner()).to.equal(owner.address);
+    });
+
+    it("Should set role codes as expected", async function () {
         expect(await minter.METADATA_ADMIN_ROLE()).to.equal("0xe02a0315b383857ac496e9d2b2546a699afaeb4e5e83a1fdef64376d0b74e5a5");
         expect(await minter.MINTER_ROLE()).to.equal("0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6");
         expect(await minter.DEFAULT_ADMIN_ROLE()).to.equal("0x0000000000000000000000000000000000000000000000000000000000000000");
+    });
+
+    it("Should assign roles as expected", async function () {
         expect(await minter.hasRole(await minter.DEFAULT_ADMIN_ROLE(), owner.address)).to.equal(true);
         expect(await minter.hasRole(await minter.MINTER_ROLE(), owner.address)).to.equal(true);
         expect(await minter.hasRole(await minter.DEFAULT_ADMIN_ROLE(), owner.address)).to.equal(true);
