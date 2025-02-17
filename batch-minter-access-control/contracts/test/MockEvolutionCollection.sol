1966a0e1a38652d34fc47c457d39294d803d393c
Toni <>
2025-02-17 10:43:59 +0100

    improving help comments

--- batch-minter-access-control/contracts/test/MockEvolutionCollection.sol
+++ batch-minter-access-control/contracts/test/MockEvolutionCollection.sol
@@ -3,6 +3,24 @@ pragma solidity ^0.8.3;
 
 import "../EvolutionCollection.sol";
 
+/**
+ * @title MockEvolutionCollection
+ * @dev A minimal mock of the precompiled contract exposed when creating 
+ * a LAOS collection via the collection factory.
+ *
+ * In tests, this mock is deployed and assigned to a fixed hardcoded 
+ * address using `hardhat_setCode`, allowing interactions with it 
+ * as if it were a real precompiled contract.
+ *
+ * However, since only the bytecode is replaced and not the contract’s state, 
+ * any state variables (like ownership) do not persist across deployments. 
+ * 
+ * As a result, ownership can only be set once—during construction—
+ * using the `immutable` keyword, which embeds its value directly 
+ * into the bytecode at deployment time.
+ */
+
+
 contract MockEvolutionCollection is EvolutionCollection {
     address immutable public owner;
 
@@ -20,7 +38,8 @@ contract MockEvolutionCollection is EvolutionCollection {
         return "42";
     }
 
-    function transferOwnership(address _newOwner) external {
-        // owner = _newOwner;
-    }
+    // Change of ownership not implemented in this mock becasue
+    // state of this contract is irrelevant.
+    // Only permissions are tested
+    function transferOwnership(address _newOwner) external {}
 }
--- batch-minter-access-control/contracts/test/MockEvolutionCollectionFactory.sol
+++ batch-minter-access-control/contracts/test/MockEvolutionCollectionFactory.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.3;
 
 import "../EvolutionCollectionFactory.sol";
 
+/**
+ * @title MockEvolutionCollectionFactory
+ * @dev A minimal mock of the precompiled code at the LAOS Network address:
+ *   0x0000000000000000000000000000000000000403
+ */
+
 contract MockEvolutionCollectionFactory is EvolutionCollectionFactory {
 
     function createCollection(address _owner) external returns (address) {
