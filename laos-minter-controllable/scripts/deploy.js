ce15543695611ccb3f4f3bcebfcfdd4b3e45879d
Toni <>
2025-02-17 11:24:51 +0100

    renamed folder

--- laos-minter-controllable/scripts/deploy.js
+++ laos-minter-controllable/scripts/deploy.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 async function main() {
 
   // The deployer account is specifed in hardhat.config.js, which reads the .env file. It needs to have funds.
-  // The owner of the batchMinter is set on deploy, and it can be any address, not necessarily the same as the deployer, not necessarily with funds.
+  // The owner of the minter is set on deploy, and it can be any address, not necessarily the same as the deployer, not necessarily with funds.
   // This owner will be able to mint, evolve, etc., for which funds will be needed.
 
   const [deployer] = await ethers.getSigners();
@@ -12,22 +12,22 @@ async function main() {
   console.log(`Deploying with account ${deployer.address}, with balance (in Wei): ${await ethers.provider.getBalance(deployer.address)}`);
 
   const LAOSMinterControlled = await ethers.getContractFactory("LAOSMinterControlled");
-  const batchMinter = await LAOSMinterControlled.deploy(owner);
-  await batchMinter.waitForDeployment();
-  console.log("...batchMinter deployed at", await batchMinter.getAddress());
+  const minter = await LAOSMinterControlled.deploy(owner);
+  await minter.waitForDeployment();
+  console.log("...minter deployed at", await minter.getAddress());
 
   // Deploy is finished. The remaining code in this script runs sanity checks, verifying that everything is correctly configured. 
 
-  console.log("...batchMinter has expected owner?", owner === await batchMinter.batchMinterOwner());
+  console.log("...minter has expected owner?", owner === await minter.batchMinterOwner());
 
-  const precompileAddress = await batchMinter.precompileAddress();
-  console.log("...batchMinter uses a collection managed by the following precompile address:", precompileAddress);
+  const precompileAddress = await minter.precompileAddress();
+  console.log("...minter uses a collection managed by the following precompile address:", precompileAddress);
 
   const EvolutionCollectionInterface = await ethers.getContractAt("EvolutionCollection", precompileAddress);
   const precompileContract = EvolutionCollectionInterface.attach(precompileAddress);
 
   const precompileOwner = await precompileContract.owner();
-  console.log("...the owner of the collection at that precompile address is the newly deployed batchMinter?", await batchMinter.getAddress() === precompileOwner);
+  console.log("...the owner of the collection at that precompile address is the newly deployed minter?", await minter.getAddress() === precompileOwner);
 }
 
 main().catch((error) => {
